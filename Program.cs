cd83c3bca9f0440934701a5d25e3c638ea9693c8
Korcsmáros Kende András <f26xc0@INF.ELTE.HU>
2023-10-03 15:18:50 +0200

    biro___megszamlalos_for_ciklus

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace RainyDay
+
+{
+    using System; //kell a bironak
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bemenet
+            int dayCount;
+
+            //Kimenet
+            int rainCount;
+
+            //Beolvasas
+            string inputText = Console.ReadLine();
+            dayCount = Convert.ToInt32(inputText);
+
+            //tömb deklaral
+            int[] rain = new int[dayCount + 1];// a +1 csak az 1tol indexeleshez kell
+            for (int i = 1; i <= dayCount; i++)
+            {
+                inputText = Console.ReadLine();
+                rain[i] = Convert.ToInt32(inputText);
+            }
+
+            //Feldolgozas
+
+            rainCount = 0;
+
+            for (int i = 1;i <= dayCount; i++)
+            {
+                if (rain[i] > 0) rainCount++;// a ++ es a valami = valami + 1 ugyanugy mukodik
+            }
+
+            //kiiras
+
+            Console.WriteLine(rainCount);
+        }
+    }
+}
